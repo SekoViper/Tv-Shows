2b2cbfabec4c0ea3983cf2c90f3873c3754ba88f
SekoVper <damehlivingstone1@gmail.com>
2022-11-28 16:54:44 +0100

    Display data from API on homepage

---
+++ src/modules/displayTVShows.js
@@ -0,0 +1,19 @@
+const tvShowsContainer = document.getElementById('shows-container');
+
+export const displayTVShows = (tvShows) => tvShows.slice(0, 12).forEach((tvShow) => {
+  const content = `
+      <div class="container">
+        <div class="tv-img">
+        <img src="${tvShow.image.medium}" alt="">
+      </div>
+      <div class="tvshows-content">
+        <p>${tvShow.name}</p>
+        <p>Rating: ${tvShow.rating.average}</p>
+      </div>
+      <button id="comment">Comment</button>
+      </div>
+  `;
+  tvShowsContainer.insertAdjacentHTML('beforeend', content);
+});
+
+export default { displayTVShows };
