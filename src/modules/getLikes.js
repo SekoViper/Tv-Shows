3187c556775b0984c9387c6cb661ab27e5645e9a
SekoVper <damehlivingstone1@gmail.com>
2022-11-29 10:57:34 +0100

    Created a file to get likes.

---
+++ src/modules/getLikes.js
@@ -0,0 +1,10 @@
+const LIKES_API_ENDPOINT = process.env.LIKESAPIPATH || '';
+
+export const getLikes = async () => {
+  const res = await fetch(LIKES_API_ENDPOINT);
+  const data = await res.json();
+  console.log(data);
+  return data;
+};
+
+export default getLikes;
