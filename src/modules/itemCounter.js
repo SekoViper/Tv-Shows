f5b25cd55e3e57e6b68900615f1284ec87371b4a
SekoVper <damehlivingstone1@gmail.com>
2022-12-01 11:19:51 +0100

    Add testing for item counter

--- src/modules/itemCounter.js
+++ src/modules/itemCounter.js
@@ -1,7 +1,7 @@
-export const counter = () => {
+const counter = () => {
   const movies = document.querySelectorAll('.main-container');
   const counterPlaceholder = document.querySelector('.counter');
   counterPlaceholder.innerHTML = movies.length;
 };
 
-export default counter;
+exports.counter = counter;
